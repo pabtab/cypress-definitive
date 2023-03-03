5fbb08263a65d5d08352e15eda2b40ec861fda09
Pablo Tabares H <pablother3@gmail.com>
2023-03-03 17:29:40 -0500

    form validation

---
+++ 03 Starting Project/cypress/e2e/contact.cy.js
@@ -0,0 +1,52 @@
+/// <reference types="Cypress" />
+
+describe("contact form", () => {
+  it("Should submit the form", () => {
+    cy.visit("http://localhost:5173/about");
+    cy.get('[data-cy="contact-input-message"]').type("message");
+    cy.get('[data-cy="contact-input-name"]').type("pablo");
+
+    cy.get('[data-cy="contact-btn-submit"]').as("submitBtn");
+    // const btn = cy.get('[data-cy="contact-btn-submit"]'); /// Not recomended and error
+
+    // cy.get("@submitBtn").contains("Send Message").should("not.have.attr", "disabled");
+    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+      expect(el.attr("disabled")).to.be.undefined;
+    });
+
+    cy.get('[data-cy="contact-input-email"]').type("test@test.com{enter}");
+    // cy.get("@submitBtn").click();
+
+    cy.get("@submitBtn").contains("Sending...").should("have.attr", "disabled");
+  });
+
+  it("should validate the form input", () => {
+    cy.visit("http://localhost:5173/about");
+    cy.get('[data-cy="contact-btn-submit"]').click();
+    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+      expect(el).to.not.have.attr("disabled");
+      expect(el.text()).to.not.equal("Sending...");
+    });
+    cy.get('[data-cy="contact-btn-submit"]').contains("Send Message");
+    cy.get('[data-cy="contact-input-message"]').blur();
+    cy.get('[data-cy="contact-input-message"]')
+      .parent()
+      .then((el) => {
+        expect(el.attr("class")).to.contains("invalid");
+      });
+
+    cy.get('[data-cy="contact-input-email"]').focus().blur();
+    cy.get('[data-cy="contact-input-email"]')
+      .parent()
+      .then((el) => {
+        expect(el.attr("class")).to.contains("invalid");
+      });
+
+    cy.get('[data-cy="contact-input-name"]').focus().blur();
+    cy.get('[data-cy="contact-input-name"]')
+      .parent()
+      .then((el) => {
+        expect(el.attr("class")).to.contains("invalid");
+      });
+  });
+});
