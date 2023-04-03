e013bea1ceaac0f9403f5ef4c122f58270c3f60f
Pablo Tabares H <pablother3@gmail.com>
2023-04-03 15:14:24 -0400

    commands and queries

--- 03 Starting Project/cypress/e2e/contact.cy.js
+++ 03 Starting Project/cypress/e2e/contact.cy.js
@@ -15,32 +15,38 @@ describe("contact form", () => {
   });
 
   it("Should submit the form", () => {
-    cy.get('[data-cy="contact-input-message"]').type("message");
-    cy.get('[data-cy="contact-input-name"]').type("pablo");
+    //  Dont run on the browser
+    cy.task("seedDatabase", "database.csv", (reutrnValue) => {});
 
-    cy.get('[data-cy="contact-btn-submit"]').as("submitBtn");
+    cy.getById("contact-input-message").type("message");
+    cy.getById("contact-input-name").type("pablo");
+
+    cy.getById("contact-btn-submit").as("submitBtn");
     // const btn = cy.get('[data-cy="contact-btn-submit"]'); /// Not recomended and error
 
     // cy.get("@submitBtn").contains("Send Message").should("not.have.attr", "disabled");
-    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+    cy.getById("contact-btn-submit").then((el) => {
       expect(el.attr("disabled")).to.be.undefined;
     });
+    cy.screenshot();
+    cy.getById("contact-input-email").type("test@test.com{enter}");
 
-    cy.get('[data-cy="contact-input-email"]').type("test@test.com{enter}");
     // cy.get("@submitBtn").click();
+    // cy.get('form button[type="submit"]').click();
+    cy.submitForm();
 
     cy.get("@submitBtn").contains("Sending...").should("have.attr", "disabled");
   });
 
   it("should validate the form input", () => {
-    cy.get('[data-cy="contact-btn-submit"]').click();
-    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+    cy.getById("contact-btn-submit").click();
+    cy.getById("contact-btn-submit").then((el) => {
       expect(el).to.not.have.attr("disabled");
       expect(el.text()).to.not.equal("Sending...");
     });
-    cy.get('[data-cy="contact-btn-submit"]').contains("Send Message");
-    cy.get('[data-cy="contact-input-message"]').blur();
-    cy.get('[data-cy="contact-input-message"]')
+    cy.getById("contact-btn-submit").contains("Send Message");
+    cy.getById("contact-input-message").blur();
+    cy.getById("contact-input-message")
       .parent()
       .should("have.attr", "class")
       .and("match", /invalid/);
@@ -50,8 +56,8 @@ describe("contact form", () => {
     //   expect(el.attr("class")).to.contains("invalid");
     // });
 
-    cy.get('[data-cy="contact-input-email"]').focus().blur();
-    cy.get('[data-cy="contact-input-email"]')
+    cy.getById("contact-input-email").focus().blur();
+    cy.getById("contact-input-email")
       .parent()
       .should("have.attr", "class")
       .and("match", /invalid/);
@@ -59,8 +65,8 @@ describe("contact form", () => {
     //   expect(el.attr("class")).to.contains("invalid");
     // });
 
-    cy.get('[data-cy="contact-input-name"]').focus().blur();
-    cy.get('[data-cy="contact-input-name"]')
+    cy.getById("contact-input-name").focus().blur();
+    cy.getById("contact-input-name")
       .parent()
       // .should("have.attr", "class")
       // .and("match", /invalid/);
